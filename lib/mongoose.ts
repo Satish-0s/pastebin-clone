43394113a17ebe01d2959051a2efa9a3f7a70dcf
Satish kumar <satishkr4548@gmail.com>
2025-12-31 03:08:10 +0530

    Fix build error by lazy-loading environment variable check

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 interface MongooseCache {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
@@ -23,12 +19,16 @@ async function dbConnect() {
         return cached.conn;
     }
 
+    if (!MONGODB_URI) {
+        throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    }
+
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
         };
 
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
